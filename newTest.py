26b2de17fcc34f37d1c2e98bee0229ca76f9b3d5
charliejindra <charlessjindra@gmail.com>
2019-07-23 06:27:32 -0400

    minor newTest changes

--- newTest.py
+++ newTest.py
@@ -6,10 +6,7 @@ print("running!")
 
 startTime = time.time()
 
-ckey = 'U6o8g2MxTxCjQXb5Ep3UrHDCc'
-csecret = 'rsv0VpttVujDpSv3gwwG61b2icfeLY8yI6xN4DZy2Z1GZJXuLU'
-atoken = '1114526834962632705-O2db2o6Ng63QCTvqyCqfL9KsH5Hh8y'
-asecret = 'yhZ3mJRDeSHuxefwp7hAy7HWqsGGwKNyMAJc55HKICE92'
+
 
 username = "vsauce"
 stream = TwitterStream(auth=OAuth(
