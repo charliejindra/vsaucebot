1dabe975ea9259e95d7dbf91c635c654fb1c15e0
charliejindra <charlessjindra@gmail.com>
2019-08-07 20:46:07 -0400

    michelChecker 1.1 alpha

--- streamMichel.py
+++ streamMichel.py
@@ -153,8 +153,8 @@ while True:
 
         except:
             print("Didn't work somewhere along the way, went to except\n")
-        mostRecentStatus =  michelChecker.user_timeline(screen_name="vsaucebot", count=1)
-        print(json.dumps(mostRecentStatus, indent=4))
+        #mostRecentStatus =  
+        print(json.dumps(michelChecker.user_timeline(screen_name="vsaucebot", count=1), indent=4))
 
 # #login to twitter account as:
 # auth = tp.OAuthHandler(consumer_key, consumer_secret)
