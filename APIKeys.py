0c3c37a7a851edc889d54dc795f7293a77efc015
charliejindra <charlessjindra@gmail.com>
2019-09-07 13:31:33 -0400

    reset api keys for the 3958674965 time

--- APIKeys.py
+++ APIKeys.py
@@ -1,4 +1,4 @@
-ckey = '2wVun8lNjVkZXKhgjCz0RsBhs'
-csecret = 'vVOQ61d9T6znOHshur5SpxyXm0giKgtXLdLzOsi97jAjOLCQE4'
-atoken = '1114526834962632705-ivvXdi5dFhtGxDn3NXjFLsNumbSSA3'
-asecret = 'FonWlnaWSeIQUxJplKWb7g8d91KcjDfSsCoo2Z8wBkqhe'
+ckey = ''
+csecret = ''
+atoken = ''
+asecret = ''
